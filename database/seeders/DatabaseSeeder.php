2ccaf045a07fb5e23c2bc00a06fa50b175fc1587
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2023-01-31 14:42:09 +0500

    Seed more content after migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,10 +35,24 @@ class DatabaseSeeder extends Seeder
 
         $user->roles()->sync([$role_admin->id]);
 
-        // Posts
+        // TODO Move posts to separate file
         $post = Post::firstOrCreate(
             [
                 'title'     => 'Hello World',
+                'author_id' => $user->id,
+            ],
+            [
+                'posted_at' => now(),
+                'content'   => "
+                    Welcome to Laravel-blog !<br><br>
+                    Don't forget to read the README before starting.<br><br>
+                    Feel free to add a star on Laravel-blog on Github !<br><br>
+                    You can open an issue or (better) a PR if something went wrong.",
+            ]
+        );
+        $post = Post::firstOrCreate(
+            [
+                'title'     => 'Hello World 2',
                 'author_id' => $user->id,
             ],
             [
